7c03788039a3ab9ad145614538efb7604417c864
maccogn <tran.n.cam@gmail.com>
2018-09-26 11:02:39 +0200

    New enemies

---
+++ src/com/codecool/snake/entities/enemies/OctopusEnemy.java
@@ -0,0 +1,53 @@
+package com.codecool.snake.entities.enemies;
+
+import com.codecool.snake.Globals;
+import com.codecool.snake.Utils;
+import com.codecool.snake.entities.Animatable;
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.geometry.Point2D;
+import javafx.scene.layout.Pane;
+
+import java.util.Random;
+
+public class OctopusEnemy extends GameEntity implements Animatable, Interactable {
+
+    private Point2D heading;
+    private static final int damage = 10;
+
+    public OctopusEnemy(Pane pane) {
+        super(pane);
+
+        setImage(Globals.octopusEnemy);
+        pane.getChildren().add(this);
+        double speed = 0.5;
+        Random rnd = new Random();
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+
+        double direction = rnd.nextDouble() * 90;
+        setRotate(direction);
+        heading = Utils.directionToVector(direction, speed);
+    }
+
+    @Override
+    public void step() {
+        if (isOutOfBounds()) {
+            destroy();
+        }
+        setX(getX() + heading.getX());
+        setY(getY() + heading.getY());
+    }
+
+    @Override
+    public void apply(SnakeHead player) {
+        player.changeHealth(-damage);
+        destroy();
+    }
+
+    @Override
+    public String getMessage() {
+        return "Octopus deal 15 damage";
+    }
+}
